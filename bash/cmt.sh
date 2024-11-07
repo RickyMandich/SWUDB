a2c652b8c12d04654a6d369d81c14dada824da8f
RickyMandich <ricky.mandich@gmail.com>
2024-11-07 14:28:19 +0100

    aggiornamento 2024 11 07 14:28

--- bash/cmt.sh
+++ bash/cmt.sh
@@ -10,7 +10,7 @@ function uploadFiles() {
     # Carica i file nella directory corrente
     for file in *; do
         if [ -f "$file" ]; then
-            echo $file
+            echo "$file"
             curl -T "$file" "ftp://swudb:Minecraft35%40@ftp.swudb.altervista.org/$file"
         fi
     done
