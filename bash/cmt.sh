56c739b7d4dd47228470ec784ffec8883ab134ad
RickyMandich <ricky.mandich@gmail.com>
2024-11-07 14:47:30 +0100

    aggiornamento 2024 11 07 14:47

--- bash/cmt.sh
+++ bash/cmt.sh
@@ -12,7 +12,7 @@ function uploadFiles() {
     for subdir in $(find . -mindepth 1 -maxdepth 1 -type d | grep -v ".git"); do
         # Calcola il nuovo percorso relativo per la sottocartella
         local newRelativePath="$relativePath/${subdir#./}"
-        uploadFiles "$subdir" "$newRelativePath"
+        uploadFiles "$subdir" "$newRelativePath/"
         # Torna alla directory precedente dopo aver processato la sottocartella
         cd ..
     done
@@ -21,7 +21,7 @@ function uploadFiles() {
     for file in *; do
         if [ -f "$file" ]; then
             echo "Caricamento di $relativePath/$file"
-            curl -T "$file" "ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21/$relativePath/$file" --ftp-pasv
+            curl -T "$file" "ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21$relativePath/$file" --ftp-pasv
         fi
     done
 }
