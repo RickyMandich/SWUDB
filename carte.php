c25e11daec439d2068dcf1f54cdabb7f2267aa4f
RickyMandich <ricky.mandich@gmail.com>
2024-11-08 12:23:47 +0100

    aggiornamento 2024 11 08 12:23

--- carte.php
+++ carte.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php preg_match("/[\/]([.*?]*[^\/]*?)$/", $_SERVER['HTTP_REFERER'], $match); ?>
+        <?php preg_match("/[\/]([.*?]*[^\/]*)$/", $_SERVER['HTTP_REFERER'], $match); ?>
         <title><?php echo $match[1][0] /* $_SERVER['HTTP_REFERER'];*/ ?></title>
         <link rel="stylesheet" href="./css/cartaPopUp.css">
         <link rel="stylesheet" href="css/mazzi.css">
