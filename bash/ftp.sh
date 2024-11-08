1302b1c9d903d9118c04705c862fbe4f0cd1c884
RickyMandich <ricky.mandich@gmail.com>
2024-11-08 18:16:08 +0100

    aggiornamento 2024 11 08 18:16

--- bash/ftp.sh
+++ bash/ftp.sh
@@ -1,15 +1,15 @@
 # Ottieni l'elenco dei file modificati nell'ultimo commit
-changedFiles=$(ls)
+changedFiles=$(find . -type f -not -path './.git/*')
 
 # Itera sui file modificati e carica ciascuno di essi
 for file in $changedFiles; do
     # Costruisci il percorso FTP per il file
-    local relativePath=$(dirname "$file")
-    local fileName=$(basename "$file")
-    local ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21/$relativePath/$fileName"
+    relativePath=$(dirname "$file" | sed 's/^\.\///')
+    fileName=$(basename "$file")
+    ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21/$relativePath/$fileName"
 
     # Esegui il comando curl per caricare il file
-    local curlCommand="curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
+    curlCommand="curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
     echo -e "$curlCommand"
     eval "$curlCommand"
     echo "$relativePath/$fileName caricato con successo."
