b1842e73b312ab0e1f7d5a9af0e57430507ccc3a
RickyMandich <ricky.mandich@gmail.com>
2024-11-08 11:46:19 +0100

    aggiornamento 2024 11 08 11:46

--- bash/cmt.sh
+++ bash/cmt.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 
-# Funzione ricorsiva per caricare i file su FTP mantenendo il percorso relativo
-function uploadFiles() {
-    local dir="$1"
-    local relativePath="$2"
-    
-    # Entra nella directory corrente
-    cd "$dir"
-    
-    # Cerca le sottocartelle
-    for subdir in $(find . -mindepth 1 -maxdepth 1 -type d | grep -v ".git"); do
-        # Calcola il nuovo percorso relativo per la sottocartella
-        local newRelativePath="$relativePath/${subdir#./}"
-        uploadFiles "$subdir" "$newRelativePath"
-        # Torna alla directory precedente dopo aver processato la sottocartella
-        cd ..
-    done
-    
-    # Carica i file nella directory corrente mantenendo il percorso relativo
-    for file in *; do
-        if [ -f "$file" ]; then
-            local ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21$relativePath/$file"
-            # Ottieni la data di modifica del file locale
-            local_modified=$(date -r "$file" +"%Y%m%d %H:%M:%S")
+# Funzione per caricare i file su FTP a partire dal commit
+function uploadFilesFromCommit() {
+    # Ottieni l'elenco dei file modificati nell'ultimo commit
+    changedFiles=$(git diff-tree --no-commit-id --name-only -r HEAD)
 
+    # Itera sui file modificati e carica ciascuno di essi
+    for file in $changedFiles; do
+        # Costruisci il percorso FTP per il file
+        local relativePath=$(dirname "$file")
+        local fileName=$(basename "$file")
+        local ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21/$relativePath/$fileName"
 
-            # Verifica la data di modifica del file remoto
-            remote_modified=$(curl -I "$ftpRequest" 2>/dev/null | grep -i "last-modified" | sed 's/Last-Modified: //g' | tr -d '\r')
-            if [ -z "$remote_modified" ]; then
-                # Il file non esiste sul server, quindi lo carica
-                echo -e "curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
-                curl -T "$file" "$ftpRequest" --ftp-pasv --ftp-create-dirs
-                echo "$relativePath/$file caricato con successo."
-            elif [ "$local_modified" \> "$remote_modified" ]; then
-                # Il file locale è più recente, quindi lo carica
-                echo -e "curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
-                curl -T "$file" "$ftpRequest" --ftp-pasv --ftp-create-dirs
-                echo "$relativePath/$file caricato con successo."
-            else
-                echo "$relativePath/$file non è stato caricato perché non è stato modificato."
-            fi
-            echo -e "\n\n"
-        fi
+        # Esegui il comando curl per caricare il file
+        local curlCommand="curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
+        echo -e "$curlCommand"
+        eval "$curlCommand"
+        echo "$relativePath/$fileName caricato con successo."
+        echo -e "\n\n"
     done
 }
 
@@ -58,8 +34,8 @@ git commit -m "$nomeCommit"
 # Esegui il push sul repository remoto
 git push
 
-# Carica i file in modo ricorsivo a partire dalla radice con percorso relativo vuoto
-uploadFiles "." ""
+# Carica i file presenti nell'ultimo commit
+uploadFilesFromCommit
 
 # sleep 5
 # clear
