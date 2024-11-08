f9f34621173c3166b4663d37eda2ce7b528602e3
RickyMandich <ricky.mandich@gmail.com>
2024-11-08 11:17:30 +0100

    aggiornamento 2024 11 08 11:17

--- bash/cmt.sh
+++ bash/cmt.sh
@@ -23,7 +23,7 @@ function uploadFiles() {
             ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21$relativePath/$file"
             # Ottieni la data di modifica del file locale
             local_modified=$(date -r "$file" +"%Y%m%d%H%M%S")
-
+            
             # Esegui il comando curl con l'opzione -z per confrontare le date di modifica
             if curl -T "$file" "$ftprequest" --ftp-pasv --ftp-create-dirs -z "$local_modified"; then
                 echo "$relativePath/$file caricato con successo."
