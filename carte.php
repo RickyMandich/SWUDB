046ff850ab8f2f9ef3e917c72c080dedc7fa4a22
RickyMandich <ricky.mandich@gmail.com>
2024-11-08 14:18:14 +0100

    aggiornamento 2024 11 08 14:18

--- carte.php
+++ carte.php
@@ -30,23 +30,22 @@
             }
         }
         require_once "header.php";
-        var_dump($_GET);
         if(count($_GET)>0 and $_GET["nome"] === 0 and $_GET["espansione"] === "tutte"): ?><meta http-equiv="refresh" content="0; url=carte"><?php endif;
-        $queryEspansione = "";
-        if(isset($_GET["espansione"])){
-            if($_GET["espansione"] !== "all"){
-                $queryEspansione = "and espansione = '".$_GET["espansione"]."' ";
+            $queryEspansione = "";
+            if(isset($_GET["espansione"])){
+                if($_GET["espansione"] !== "all"){
+                    $queryEspansione = "and espansione = '".$_GET["espansione"]."' ";
+                }
             }
-        }
-        $leader = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo = 'leader' order by uscita, espansione, numero");
-        $basi = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo = 'base' order by uscita, espansione, numero");
-        $altro = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo <> 'leader' and tipo <> 'base' order by uscita, espansione, numero");
-        $rs = [];
-        f($leader, $rs);
-        f($basi, $rs);
-        f($altro, $rs);
-        $conn->close();
-    ?>
+            $leader = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo = 'leader' order by uscita, espansione, numero");
+            $basi = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo = 'base' order by uscita, espansione, numero");
+            $altro = $conn->query("select * from carte where nome like '%" . ($_GET["nome"] ?? "") . "%' ".$queryEspansione."and tipo <> 'leader' and tipo <> 'base' order by uscita, espansione, numero");
+            $rs = [];
+            f($leader, $rs);
+            f($basi, $rs);
+            f($altro, $rs);
+            $conn->close();
+            ?>
     <body>
         <div class="container">
             <form action="carte">
@@ -63,48 +62,50 @@
             </form>
             <div class="decks-section">
                 <div class="decks-container">
-                    <?php if(count($rs)> 0): ?>
-                    <table>
-                        <thead>
-                            <tr class="deck-header">
-                            <?php if(isset($_SESSION["user"])): ?>
-                                <td>aggiungi alla collezione</td>
-                            <?php endif; ?>
-                                <?php foreach($rs[0] as $column): ?>
-                                    <td>
-                                        <?php echo $column; ?>
-                                    </td>
-                                <?php endforeach; ?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php for($i = 1;$i < count($rs); $i++): ?>
-                            <tr class="card-in-deck-row deck-card">
+                    <?php if(true) : ?>
+                        <?php var_dump($_GET);?>
+                    <?php elseif(count($rs)> 0): ?>
+                        <table>
+                            <thead>
+                                <tr class="deck-header">
                                 <?php if(isset($_SESSION["user"])): ?>
-                                    <td style="max-width: 100vw">
-                                        <form action="./insertTo" method="get">
-                                            <input type="hidden" name="mazzo" value="Collezione">
-                                            <input type="hidden" name="espansione" value="<?php echo $rs[$i]["espansione"]?>">
-                                            <input type="hidden" name="numero" value="<?php echo $rs[$i]["numero"]?>">
-                                            <input type="hidden" name="from" value="<?php echo ".".$_SERVER['REQUEST_URI'];?>">
-                                            <input type="image" src='img/collezione.png' width='100px' height='auto' alt="Invia il form">
-                                        </form>
-                                    </td>
+                                    <td>aggiungi alla collezione</td>
                                 <?php endif; ?>
-                                <?php foreach($rs[$i] as $value): ?>
-                                <td>
-                                    <a href="<?php echo "https://swudb.com/card/" . $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>" target="_blank">
-                                        <?php echo $value;
-                                            if($value === $rs[$i]["nome"]){
-                                                ?><img class="card-hover" src="https://swudb.com/cards/<?php echo $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>.png"><?php
-                                            }; ?>
-                                    </a>
-                                </td>
-                                <?php endforeach; ?>
-                            </tr>
-                            <?php endfor; ?>
-                        </tbody>
-                    </table>
+                                    <?php foreach($rs[0] as $column): ?>
+                                        <td>
+                                            <?php echo $column; ?>
+                                        </td>
+                                    <?php endforeach; ?>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php for($i = 1;$i < count($rs); $i++): ?>
+                                <tr class="card-in-deck-row deck-card">
+                                    <?php if(isset($_SESSION["user"])): ?>
+                                        <td style="max-width: 100vw">
+                                            <form action="./insertTo" method="get">
+                                                <input type="hidden" name="mazzo" value="Collezione">
+                                                <input type="hidden" name="espansione" value="<?php echo $rs[$i]["espansione"]?>">
+                                                <input type="hidden" name="numero" value="<?php echo $rs[$i]["numero"]?>">
+                                                <input type="hidden" name="from" value="<?php echo ".".$_SERVER['REQUEST_URI'];?>">
+                                                <input type="image" src='img/collezione.png' width='100px' height='auto' alt="Invia il form">
+                                            </form>
+                                        </td>
+                                    <?php endif; ?>
+                                    <?php foreach($rs[$i] as $value): ?>
+                                    <td>
+                                        <a href="<?php echo "https://swudb.com/card/" . $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>" target="_blank">
+                                            <?php echo $value;
+                                                if($value === $rs[$i]["nome"]){
+                                                    ?><img class="card-hover" src="https://swudb.com/cards/<?php echo $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>.png"><?php
+                                                }; ?>
+                                        </a>
+                                    </td>
+                                    <?php endforeach; ?>
+                                </tr>
+                                <?php endfor; ?>
+                            </tbody>
+                        </table>
                     <?php else: ?>
                         nessuna carta trovata
                     <?php endif; ?>
