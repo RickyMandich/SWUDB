af5eabc2f4bfc1bee183445cd4561df5d0d1d4f2
RickyMandich <ricky.mandich@gmail.com>
2024-11-09 10:43:56 +0100

    aggiornamento 2024 11 09 10:43

--- query.php
+++ query.php
@@ -18,35 +18,69 @@ if(isset($_SESSION["user"]) && unserialize($_SESSION["user"])->getID() === 0):?>
     if($select):
         $resultSet = $conn->query($_GET["query"]);
     ?>
-    <table border="">
-        <thead>
-            <tr>
-                <?php foreach ($firstLine = $resultSet->fetch_assoc() as $key => $row): ?>
-                    <td>
-                        <?php echo $key; ?>
-                    </td>
+    <div class="container">
+        <form action="carte">
+            <input type="text" name="nome" value="<?php echo $_GET["nome"] ?? ""?>">
+            <select multiple name="espansione[]" id="espansione">
+                <option <?php if(isset($_GET["espansione"]) and in_array("all", $_GET["espansione"])) echo "selected" ?>>all</option>
+                <?php foreach($espansioni as $set) : ?>
+                    <option <?php if(isset($_GET["espansione"]) and in_array($set, $_GET["espansione"])) echo "selected" ?>>
+                        <?php echo $set ?>
+                    </option>
                 <?php endforeach; ?>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach($firstLine as $value): ?>
-                    <td>
-                        <?php echo $value; ?>
-                    </td>
-                <?php endforeach; ?>
-            </tr>
-            <?php while($line = $resultSet->fetch_assoc()): ?>
-                <tr>
-                    <?php foreach($line as $value): ?>
-                        <td>
-                            <?php echo $value; ?>
-                        </td>
-                    <?php endforeach; ?>
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
+            </select>
+            <input type="submit" value="cerca">
+        </form>
+        <div class="decks-section">
+            <div class="decks-container">
+                <?php if(count($rs)> 0): ?>
+                    <table>
+                        <thead>
+                            <tr class="deck-header">
+                            <?php if(isset($_SESSION["user"])): ?>
+                                <td>aggiungi alla collezione</td>
+                            <?php endif; ?>
+                                <?php foreach($rs[0] as $column): ?>
+                                    <td>
+                                        <?php echo $column; ?>
+                                    </td>
+                                <?php endforeach; ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php for($i = 1;$i < count($rs); $i++): ?>
+                            <tr class="card-in-deck-row deck-card">
+                                <?php if(isset($_SESSION["user"])): ?>
+                                    <td style="max-width: 100vw">
+                                        <form action="./insertTo" method="get">
+                                            <input type="hidden" name="mazzo" value="Collezione">
+                                            <input type="hidden" name="espansione" value="<?php echo $rs[$i]["espansione"]?>">
+                                            <input type="hidden" name="numero" value="<?php echo $rs[$i]["numero"]?>">
+                                            <input type="hidden" name="from" value="<?php echo ".".$_SERVER['REQUEST_URI'];?>">
+                                            <input type="image" src='img/collezione.png' width='100px' height='auto' alt="Invia il form">
+                                        </form>
+                                    </td>
+                                <?php endif; ?>
+                                <?php foreach($rs[$i] as $value): ?>
+                                <td>
+                                    <a href="<?php echo "https://swudb.com/card/" . $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>" target="_blank">
+                                        <?php echo $value;
+                                            if($value === $rs[$i]["nome"]){
+                                                ?><img class="card-hover" src="https://swudb.com/cards/<?php echo $rs[$i]["espansione"] . "/" . sprintf("%0" . $numeri[$rs[$i]["espansione"]] . "d", $rs[$i]["numero"]);?>.png"><?php
+                                            }; ?>
+                                    </a>
+                                </td>
+                                <?php endforeach; ?>
+                            </tr>
+                            <?php endfor; ?>
+                        </tbody>
+                    </table>
+                <?php else: ?>
+                    nessuna carta trovata
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
     <?php else:
         echo "ho fatto ".$conn->affected_rows." modifiche";
         endif;
