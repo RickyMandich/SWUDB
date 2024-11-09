076ee89a351912c3e3f5c17813a2d8a3b53cb3c9
RickyMandich <ricky.mandich@gmail.com>
2024-11-09 15:07:40 +0100

    aggiornamento 2024 11 09 15:07

--- query.php
+++ query.php
@@ -34,7 +34,7 @@ require_once("header.php");?>
                     $altro = $conn -> query($queryAltro);
                 }
                 $rs = $conn->query($_GET["query"]);
-                echo $resultSet;
+                echo "|$resultSet|";
                 if($resultSet->num_rows>0):?>
                     <form action="./query" method="get">
                         <input type="text" name="query" id="query" value="<?php if(isset($_GET["query"])) echo $_GET["query"]; else echo "select * from "; ?>">
