a8466948fe818920667072066f9ef89ec14c9052
RickyMandich <ricky.mandich@gmail.com>
2024-11-11 16:51:53 +0100

    aggiornamento 2024 11 11 16:51

--- mazzi.php
+++ mazzi.php
@@ -79,6 +79,7 @@
                                             </form>
                                         <?php else: ?>
                                             <img src="https://swudb.com/cards/<?php echo $row["espansione"]."/".sprintf("%0".$numeri[$leader["espansione"]]."d", $row["numero"])."-portrait.png";?>" width="100vw">
+                                            <?php echo "https://swudb.com/cards/".$row["espansione"]."/".sprintf("%0".$numeri[$leader["espansione"]]."d", $row["numero"])."-portrait.png"?>
                                         <?php endif; ?>
                                     </td>
                                     <td style="max-width: 100vw">
