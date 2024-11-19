9aa4128e44406778491ce730c702cbabf9639c07
RickyMandich <ricky.mandich@gmail.com>
2024-11-19 17:21:19 +0100

    aggiornamento 2024 11 19 17:21

--- inputJsonDeck.php
+++ inputJsonDeck.php
@@ -23,6 +23,7 @@
                     if ($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
                     }
+                    echo $deck->getInsertSql();
                     $conn->query($deck->getInsertSql());
                     echo "caricamento riuscito, ho inserito ".$conn->affected_rows." carte";
                     ?><meta http-equiv="refresh" content="3; url=inputJsonDeck"><?php
