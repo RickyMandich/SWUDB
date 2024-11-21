c551910c70749765a0848e456c2bb7dab32e6a92
RickyMandich <ricky.mandich@gmail.com>
2024-11-21 16:09:06 +0100

    aggiornamento 2024 11 21 16:09

--- insertTo.php
+++ insertTo.php
@@ -79,7 +79,7 @@
         if(str_starts_with($_GET["from"], "./carte")): ?>
             <meta http-equiv="refresh" content="0; url=<?php echo $_GET["from"];?>">
         <?php else: ?>
-        <meta http-equiv="refresh" content="; url=insertTo<?php echo $_GET["from"].'?'.http_build_query(array('resultClass' => $resultClass, 'resultText' => $resultText, 'espansione' => $_GET["espansione"], 'numero' => $_GET["numero"], 'mazzo' => $_GET["mazzo"]));?>">
+        <meta http-equiv="refresh" content="15; url=insertTo<?php echo $_GET["from"].'?'.http_build_query(array('resultClass' => $resultClass, 'resultText' => $resultText, 'espansione' => $_GET["espansione"], 'numero' => $_GET["numero"], 'mazzo' => $_GET["mazzo"]));?>">
         <?php endif; ?>
     </body>
 </html>
