eb2a74fcf95ee584548483c3dbaf9faac417840b
RickyMandich <ricky.mandich@gmail.com>
2024-11-23 16:12:24 +0100

    aggiornamento 2024 11 23 16:12

--- moveTo.php
+++ moveTo.php
@@ -2,7 +2,8 @@
     require_once "header.php";
     if(isset($_GET["from"])){
         $query = "insert into mazzi\n values('".$_GET["into"]."', '".$_GET["espansione"]."', ".$_GET["numero"].", ".unserialize($_SESSION["user"])->getID().", ".$_GET["foil"].", ".$_GET["public"].")";
-        echo $query;
+        var_dump($query);
+        var_dump($get);
         $conn-> query($query);
         ?><meta http-equiv="refresh" content="0; url=<?php echo "remove?".http_build_query($_GET);?>"><?php
     }
