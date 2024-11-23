2cf9de642e1c7164fb05a2c4ab7f1ced6d31ae0e
RickyMandich <ricky.mandich@gmail.com>
2024-11-23 19:38:32 +0100

    aggiornamento 2024 11 23 19:38

--- mazzi.php
+++ mazzi.php
@@ -124,6 +124,7 @@
                                     <td>
                                         <form action="exportDeck">
                                             <input type="hidden" name="mazzo" value="<?php $row["mazzo"];?>">
+                                            <img src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nLzEyOC9t/aXNjZWxsYW5lb3Vz/L2Vhc2Vtb2ItaWNv/bi9leHBvcnQtZmls/ZS0xLnBuZw" alt="export as text">
                                         </form>
                                     </td>
                                     <?php foreach($row as $key=>$cell): ?>
