bdf9b0c7c361ae5a49912d651a537ea649a33f8b
RickyMandich <ricky.mandich@gmail.com>
2024-11-23 19:50:15 +0100

    aggiornamento 2024 11 23 19:50

--- exportDeck.php
+++ exportDeck.php
@@ -17,7 +17,8 @@ require_once "header.php";
 echo "select c.espansione, c.numero, c.nome, c.titolo from carte c, mazzi m where m.espansione = c.espansione and m.numero = c.numero and m.mazzo = '".$_GET["mazzo"]."'";
 $resultSet = $conn -> query("select c.espansione, c.numero, c.nome, c.titolo from carte c, mazzi m where m.espansione = c.espansione and m.numero = c.numero and m.mazzo = '".$_GET["mazzo"]."'");
 $result = $_GET["mazzo"]."\n";
+var_dump($result);
 while($line = $resultSet->fetch_assoc()){
     $result = $result.$line["espansione"]."_".$line["numero"]."\t".strtoupper($line["nome"])." ".$line["titolo"]."\n";
 }
-download_file($result, $_GET["mazzo"].".txt");
+//download_file($result, $_GET["mazzo"].".txt");
