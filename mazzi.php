d1b78a385fd8d1feff2a36f74576d2a98fd7332c
RickyMandich <ricky.mandich@gmail.com>
2024-11-24 22:11:18 +0100

    aggiornamento 2024 11 24 22:11

--- mazzi.php
+++ mazzi.php
@@ -125,7 +125,7 @@
                                         <?php if(!isset($precedente) or $row["mazzo"] !== $precedente){?>
                                             <form action="exportDeck">
                                                 <input type="hidden" name="mazzo" value="<?php echo $row["mazzo"];?>">
-                                                <input type="image" src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nLzEyOC9t/aXNjZWxsYW5lb3Vz/L2Vhc2Vtb2ItaWNv/bi9leHBvcnQtZmls/ZS0xLnBuZw" alt="export as text">
+                                                <input type="image" src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nLzEyOC9t/aXNjZWxsYW5lb3Vz/L2Vhc2Vtb2ItaWNv/bi9leHBvcnQtZmls/ZS0xLnBuZw" alt="export as text" class="exportDeck">
                                             </form>
                                         <?php }else{?>
                                             <form action="moveTo">
@@ -135,7 +135,7 @@
                                                 <input type="hidden" name="foil" value="<?php echo $row["foil"]?>">
                                                 <input type="hidden" name="public" value="1">
                                                 <input type="hidden" name="from" value="mazzi">
-                                                <input type="image" src="https://imgs.search.brave.com/tOlbrzqxPM8E8cIRWHPtdsVrBcMPZfG3NHK4TIWZJoc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZHJ5aWNvbnMu/Y29tL3VwbG9hZHMv/aWNvbi9wcmV2aWV3/Lzc0MTQvc21hbGxf/MXhfbGlzdC5wbmc" alt="mancante" class="exportDeck">
+                                                <input type="image" src="https://imgs.search.brave.com/tOlbrzqxPM8E8cIRWHPtdsVrBcMPZfG3NHK4TIWZJoc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZHJ5aWNvbnMu/Y29tL3VwbG9hZHMv/aWNvbi9wcmV2aWV3/Lzc0MTQvc21hbGxf/MXhfbGlzdC5wbmc" alt="mancante">
                                             </form>
                                         <?php }?>
                                     </td>
