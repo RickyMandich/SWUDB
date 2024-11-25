4f3c45c951ff0ee0cfec977f0f0018eddcef1048
RickyMandich <ricky.mandich@gmail.com>
2024-11-25 11:05:42 +0100

    aggiornamento 2024 11 25 11:05

--- mazzi.php
+++ mazzi.php
@@ -26,6 +26,7 @@
             $row = [];
             foreach($line as $key => $value){
                 if($key === 'mazzo' and $line["codUtente"] !== unserialize($_SESSION["user"])->getID()){
+                    echo $line["codUtente"]." !== ".unserialize($_SESSION["user"])->getID()."<br>";
                     $value = $value." di ".$conn->query("select nome from utenti where id = ".$line["codUtente"])->fetch_assoc()["nome"];
                 }
                 $row[$key] = $value;
