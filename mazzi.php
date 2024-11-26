f1d8ea96bcc7632b030d6dd6aac5538a2355f7b0
RickyMandich <ricky.mandich@gmail.com>
2024-11-26 16:57:24 +0100

    aggiornamento 2024 11 26 16:57

--- mazzi.php
+++ mazzi.php
@@ -25,6 +25,7 @@
                     array_push($mazzoOrder, $line["mazzo"]." di ".$conn->query("select nome from utenti where id = ".$line["codUtente"])->fetch_assoc()["nome"]);
                 }
             }
+            $_SESSION["tmp"] = $mazzoOrder;
             // Definisco l'ordine dei tipi
             $tipoOrder = ['Leader', 'Base'];
             
@@ -228,6 +229,7 @@
     ?>
     <body>
         <div class="container">
+            <?php echo $_SESSION["tmp"];?>
             <div class="decks-section">
                 <h2>I Tuoi Mazzi <?php echo $conn->query("select nome from utenti where id = ".unserialize($_SESSION["user"])->getID())->fetch_assoc()["nome"]?></h2>
                 <div class="decks-container">
