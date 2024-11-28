a728b7ac0141c99a0b2634f01c5fe60be2b93080
RickyMandich <ricky.mandich@gmail.com>
2024-11-28 17:22:46 +0100

    aggiornamento 2024 11 28 17:22

--- mazzi.php
+++ mazzi.php
@@ -10,7 +10,7 @@
     </head>
     <?php
         require_once "header.php";
-        require_once "orederCard";
+        require_once "orderCard";
         if (!isset($_SESSION["user"])){
             echo $file;
             ?>
