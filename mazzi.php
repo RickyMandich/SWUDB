96e619c2470561c3e5fc47b96bf38686d2ea1e60
RickyMandich <ricky.mandich@gmail.com>
2024-11-28 16:28:29 +0100

    aggiornamento 2024 11 28 16:28

--- mazzi.php
+++ mazzi.php
@@ -30,7 +30,8 @@
 
             $getNumero = function($el) use ($conn){
                 echo "select numero from carte where espansione = '".$el["espansione"]."' and nome like '".$el["nome"]."' and titolo like ".$el["titolo"]."'";
-                return $conn->query("select numero from carte where espansione = '".$el["espansione"]."' and nome like '".$el["nome"]."' and titolo like ".$el["titolo"]."'")->fetch_assoc()["nome"];
+                echo "<br>".$conn->query("select numero from carte where espansione = '".$el["espansione"]."' and nome like '".$el["nome"]."' and titolo like ".$el["titolo"]."'")->fetch_assoc()["nome"];
+                exit;
             };
             
             // Definisco l'ordine degli aspetti primari
