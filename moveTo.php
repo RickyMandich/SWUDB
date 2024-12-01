d77e07b4b5e5b64cbd9124abce889b03a7065e65
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 23:47:42 +0100

    aggiornamento 2024 12 01 23:47

--- moveTo.php
+++ moveTo.php
@@ -2,4 +2,5 @@
     require_once "header.php";
     if(isset($_GET["from"])){
         moveTo($_GET["into"], $_GET["espansione"], $_GET["numero"], $_GET["foil"], $_GET["mazzo"]);
+        ?><meta http-equiv="refresh" content="0; url=./<?php echo $_GET["from"]?>"><?
     }
