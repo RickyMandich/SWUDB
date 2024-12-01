b3d17dfc37440507816b2b979e2dc2d36954a711
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 17:26:20 +0100

    aggiornamento 2024 12 01 17:26

--- variabili.php
+++ variabili.php
@@ -42,7 +42,7 @@
         }
         return $quante;
     }
-    
+
     function insertTo($espansione, $numero, $mazzo, $foil){
         $espansione = strtoupper($espansione);
         if(exist($espansione, $numero)){
@@ -51,7 +51,6 @@
                 if(!$id = $id->fetch_assoc()){
                     $GLOBALS["conn"]->query("insert into mazzi (nome, public, codUtente) values('".$mazzo."', 0, ".unserialize($_SESSION["user"])->getID().");");
                 }
-                echo "insert into composizione (idMazzo, espansione, numero, foil) values(".$id["id"].", '".$espansione."', ".$numero.", ".($foil === 'on' ? "true" : "false").");";
                 $result = $GLOBALS["conn"]->query("insert into composizione (idMazzo, espansione, numero, foil) values(".$id["id"].", '".$espansione."', ".$numero.", ".($foil === 'on' ? "1" : "0").");");
                 if($result === true){
                     $resultClass = "success";
