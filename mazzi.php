e30f31b86ea8b70b75bb4439fcb0ef8734f974d1
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 13:55:59 +0100

    aggiornamento 2024 12 01 13:55

--- mazzi.php
+++ mazzi.php
@@ -57,7 +57,7 @@
     ?>
     <body>
         <div class="container">
-            <!-- <table>
+            <table>
                 <thead>
                     <?php foreach($preDeck as $carta):?>
                         <tr>
@@ -67,7 +67,7 @@
                         </tr>
                     <?php endforeach;?>
                 </thead>
-            </table> -->
+            </table>
             <div class="decks-section">
                 <h2>I Tuoi Mazzi <?php echo $conn->query("select nome from utenti where id = ".unserialize($_SESSION["user"])->getID())->fetch_assoc()["nome"]?></h2>
                 <div class="decks-container">
