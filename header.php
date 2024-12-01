dea9bd36561d1b5d7a8187911c6b5ac1d2e28b97
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 15:49:45 +0100

    aggiornamento 2024 12 01 15:49

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
             foreach($line as $i => $value){
                 unset($j);
                 for($j = 0;$j<=$deep;$j++){
-                    echo "\t";
+                    echo "&nbsp;";
                 }
                 echo "$i-->";
                 printlnd($value, $deep+1);
