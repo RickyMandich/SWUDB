0cf0173425ffa3cc529d03056a3acb45176e70e7
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 15:14:46 +0100

    aggiornamento 2024 12 01 15:14

--- header.php
+++ header.php
@@ -6,13 +6,12 @@
     require_once "classi/Cards.php";
     require_once "variabili.php";
     function printlnd($line, $deep){
-        echo "<br>";
         for($i = 0;$i<$deep;$i++){
             echo "--";
         }
-        echo "$line->".gettype($line);
         if(gettype($line) == 'array'){
             foreach($line as $i => $value){
+                echo $i;
                 println($value);
             }
         }else{
