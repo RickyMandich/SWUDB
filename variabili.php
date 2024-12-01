ca908a7e886aafac417f681d1247c6c8542ac4f9
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 23:33:49 +0100

    aggiornamento 2024 12 01 23:33

--- variabili.php
+++ variabili.php
@@ -51,7 +51,8 @@
                 if(!$id = $id->fetch_assoc()){
                     $GLOBALS["conn"]->query("insert into mazzi (nome, public, codUtente) values('".$mazzo."', 0, ".unserialize($_SESSION["user"])->getID().");");
                 }
-                $result = $GLOBALS["conn"]->query("insert into composizione (idMazzo, espansione, numero, foil) values(".$id["id"].", '".$espansione."', ".$numero.", ".($foil === 'on' ? "1" : "0").");");
+                echo "insert into composizione (idMazzo, espansione, numero, foil) values(".$id["id"].", '$espansione', $numero, ".($foil === 'on' ? "1" : "0").");";
+                $result = $GLOBALS["conn"]->query("insert into composizione (idMazzo, espansione, numero, foil) values(".$id["id"].", '$espansione', $numero, ".($foil === 'on' ? "1" : "0").");");
                 if($result === true){
                     $resultClass = "success";
                     $resultText = "carta aggiunta";
