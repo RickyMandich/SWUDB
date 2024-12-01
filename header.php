ca5f8e1ab04e7f0f65b4daaf2b143f90c2352877
RickyMandich <ricky.mandich@gmail.com>
2024-12-01 15:43:21 +0100

    aggiornamento 2024 12 01 15:43

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         if(gettype($line) == 'array'){
             echo "{<br>";
             foreach($line as $i => $value){
-                echo $deep>0?"|":"";
+                echo $deep>-1?"|":"";
                 unset($j);
                 for($j = 0;$j<=$deep;$j++){
                     echo "--";
