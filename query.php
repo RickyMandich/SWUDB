b60e6c65db293118129ce9ed437d27ba58ace2ae
RickyMandich <ricky.mandich@gmail.com>
2024-12-07 14:57:13 +0100

    aggiornamento 2024 12 07 14:57

--- query.php
+++ query.php
@@ -45,12 +45,16 @@ require_once("header.php");?>
                                         <tr class="card-in-deck-row deck-card">
                                             <?php foreach($resultSet as $value): ?>
                                                 <td>
+                                                    <?php if(isset($resultSet["nome"]) and isset($resultSet["espansione"]) and isset($resultSet["numero"])){?>}
                                                     <a href="<?php echo "https://swudb.com/card/" . $resultSet["espansione"] . "/" . sprintf("%0" . $numeri[$resultSet["espansione"]] . "d", $resultSet["numero"]);?>" target="_blank">
                                                         <?php echo "---$value---";
                                                             if($value === $resultSet["nome"]){
                                                                 ?><img class="card-hover" src="https://swudb.com/cards/<?php echo $resultSet["espansione"] . "/" . sprintf("%0" . $numeri[$resultSet["espansione"]] . "d", $resultSet["numero"]);?>.png"><?php
                                                             }; ?>
                                                     </a>
+                                                    <?php }else{
+                                                        echo $value;
+                                                    }?>
                                                 </td>
                                             <?php endforeach; ?>
                                         </tr>
