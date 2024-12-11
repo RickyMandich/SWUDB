acdf1b97c8a74c9a60f5093fe305f3b420b79b04
RickyMandich <ricky.mandich@gmail.com>
2024-12-11 18:00:03 +0100

    aggiornamento 2024 12 11 18:00

--- inputJsonDeck.php
+++ inputJsonDeck.php
@@ -12,7 +12,7 @@
             $deck->createDeck(isset($_GET["public"]) ? ($_GET["public"] =="on"?"1":"0") : "0");
             foreach($deck->carte as $c){
                 $query = getQueryCartaFromCollezione($c, "Collezione");
-                echo str_replace("\n", "<br>", str_replace("    ", "&nbsp;&nbsp;&nbsp;&nbsp;", $query)."<br>");
+                //echo str_replace("\n", "<br>", str_replace("    ", "&nbsp;&nbsp;&nbsp;&nbsp;", $query)."<br>");
                 $query = $GLOBALS["conn"]->query($query)->fetch_assoc();
                 var_dump($query);
                 echo "<br>";
