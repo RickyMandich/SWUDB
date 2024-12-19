ebe6e2302f866bc474501f4509c95b35d2e87a4e
RickyMandich <ricky.mandich@gmail.com>
2024-12-19 16:17:38 +0100

    aggiornamento 2024 12 19 16:17

--- insertToDeck.php
+++ insertToDeck.php
@@ -20,11 +20,11 @@
                     $rs = $GLOBALS["conn"]->query("select nome as mazzo from mazzi where codUtente = ".unserialize($_SESSION["user"])->getID()." and not nome like 'mancanti di %' order by nome");
                     $mazzi = [];
                     while($line = $rs->fetch_assoc()){
-                        echo $line["mazzo"];
                         array_push($mazzi, $line["mazzo"]);
                     }
                 ?>
                 <?php foreach($mazzi as $m):?>
+                    <?php echo $m;?>
                     <option value="<?php $m?>">
                 <?php endforeach;?>
             </datalist>
