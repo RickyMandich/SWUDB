62e35700b2ecaf7389f566b6a23c5504b00ae540
RickyMandich <ricky.mandich@gmail.com>
2024-12-25 19:21:04 +0100

    aggiornamento 2024 12 25 19:21

--- profilo.php
+++ profilo.php
@@ -52,24 +52,17 @@
                         </td>
                     </tr>
                     <?php foreach($mazzi as $value):
-                        if($value!=="Collezione"):?>
+                        $leader = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='leader' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'");
+                        $base = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='base' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'");
+                        if($value!=="Collezione" and $leader = $leader->fetch_assoc() and $base = $base->fetch_assoc()):?>
                             <tr class="deck-card">
                                 <td>
                                     <?php echo $value; ?>
                                 </td>
                                 <td>
-                                    <?php $leader = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='leader' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'")->fetch_assoc();
-                                    echo "select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='leader' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'<br>";
-                                    echo $leader["espansione"]."_".$leader["numero"]."<br>";
-                                    var_dump($leader);?>
-
                                     <img src="https://swudb.com/cards/<?php echo $leader["espansione"]."/".sprintf("%0". $numeri[$leader["espansione"]]."d", $leader["numero"]).".png"?>" alt="<?php echo $leader["nome"];?>">
                                 </td>
                                 <td>
-                                    <?php $base = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='base' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'")->fetch_assoc();
-                                    echo $leader["espansione"]."_".$leader["numero"]."<br>";
-                                    var_dump($leader);?>
-
                                     <img src="https://swudb.com/cards/<?php echo $base["espansione"]."/".sprintf("%0". $numeri[$base["espansione"]]."d", $base["numero"]).".png"?>" alt="<?php echo $base["nome"];?>">
                                 </td>
                             </tr>
