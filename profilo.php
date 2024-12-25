03b97cefc88d1a5faa6596578a99a68bc7818481
RickyMandich <ricky.mandich@gmail.com>
2024-12-25 19:09:44 +0100

    aggiornamento 2024 12 25 19:09

--- profilo.php
+++ profilo.php
@@ -62,7 +62,7 @@
 
                                     <img src="https://swudb.com/cards/<?php echo $leader["espansione"]."/".sprintf("%0". $numeri[$leader["espansione"]]."d", $leader["numero"]).".png"?>" alt="<?php echo $leader["nome"];?>">
                                 </td>
-                                <?php $base = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione m where c.tipo='base' and m.espansione = c.espansione and m.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'")->fetch_assoc();?>
+                                <?php $base = $GLOBALS["conn"]->query("select c.nome, c.espansione, c.numero from carte c, composizione co, mazzi m where co.idMazzo = m.id and c.tipo='base' and co.espansione = c.espansione and co.numero = c.numero and m.codUtente = ".unserialize($_SESSION["user"])->getID()." and m.nome = '".$value."'")->fetch_assoc();?>
                                 <td>
                                 <img src="https://swudb.com/cards/<?php echo $base["espansione"]."/".sprintf("%0". $numeri[$base["espansione"]]."d", $base["numero"]).".png"?>" alt="<?php echo $base["nome"];?>">
                                 </td>
