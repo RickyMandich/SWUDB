41c27f20ba0771846b3ebb6673d3ed5a12a49b39
RickyMandich <ricky.mandich@gmail.com>
2025-01-09 11:10:53 +0100

    aggiornamento 2025 01 09 11:10

--- mazzi.php
+++ mazzi.php
@@ -89,6 +89,8 @@
                                             <?php else: ?>
                                                 <img src="https://swudb.com/cards/<?php echo $row["espansione"]."/".sprintf("%0".$numeri[$row["espansione"]]."d", $row["numero"])."-portrait.png";?>" width="100vw">
                                             <?php endif; ?>
+                                        <?php elseif(!isset($precedente) or $row["mazzo"] !== $precedente):?>
+                                            <img src="https://swudb.com/cards/<?php echo $row["espansione"]."/".sprintf("%0".$numeri[$row["espansione"]]."d", $row["numero"]).".png";?>" height="100vh">
                                         <?php endif;?>
                                     </td>
                                     <td style="max-width: 100vw">
@@ -124,31 +126,31 @@
                                                         <input type="submit" value="crea nuovo mazzo">
                                                     </form>
                                                 </div>
-                                                <?php endif;?>
                                             <?php else: ?>
                                                 <img src="https://swudb.com/cards/<?php echo $row["espansione"]."/".sprintf("%0".$numeri[$row["espansione"]]."d", $row["numero"]).".png";?>" height="100vh">
                                             <?php endif; ?>
+                                        <?php elseif(!isset($precedente) or $row["mazzo"] !== $precedente):?>
+                                            <img src="https://swudb.com/cards/<?php echo $row["espansione"]."/".sprintf("%0".$numeri[$row["espansione"]]."d", $row["numero"]).".png";?>" height="100vh">
+                                        <?php endif;?>
                                     </td>
                                     <td>
-                                        <?php if(unserialize($_SESSION["user"])->getID() == $row["codUtente"]):?>
-                                            <?php if(!isset($precedente) or $row["mazzo"] !== $precedente){?>
-                                                <form action="exportDeck">
-                                                    <input type="hidden" name="mazzo" value="<?php echo $row["mazzo"];?>">
-                                                    <input type="image" src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nLzEyOC9t/aXNjZWxsYW5lb3Vz/L2Vhc2Vtb2ItaWNv/bi9leHBvcnQtZmls/ZS0xLnBuZw" alt="export as text" class="exportDeck">
-                                                </form>
-                                            <?php }else{?>
-                                                <form action="moveTo">
-                                                    <input type="hidden" name="mazzo" value="<?php echo $row["mazzo"]?>">
-                                                    <input type="hidden" name="espansione" value="<?php echo $row["espansione"]?>">
-                                                    <input type="hidden" name="numero" value="<?php echo $row["numero"]?>">
-                                                    <input type="hidden" name="foil" value="<?php echo $row["foil"]?>">
-                                                    <input type="hidden" name="into" value="mancanti di <?php echo $row["mazzo"];?>">
-                                                    <input type="hidden" name="public" value="1">
-                                                    <input type="hidden" name="from" value="mazzi">
-                                                    <input type="image" src="https://imgs.search.brave.com/tOlbrzqxPM8E8cIRWHPtdsVrBcMPZfG3NHK4TIWZJoc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZHJ5aWNvbnMu/Y29tL3VwbG9hZHMv/aWNvbi9wcmV2aWV3/Lzc0MTQvc21hbGxf/MXhfbGlzdC5wbmc" alt="mancante">
-                                                </form>
-                                            <?php }?>
-                                        <?php endif;?>
+                                        <?php if(!isset($precedente) or $row["mazzo"] !== $precedente){?>
+                                            <form action="exportDeck">
+                                                <input type="hidden" name="mazzo" value="<?php echo $row["mazzo"];?>">
+                                                <input type="image" src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29u/cy52ZXJ5aWNvbi5j/b20vcG5nLzEyOC9t/aXNjZWxsYW5lb3Vz/L2Vhc2Vtb2ItaWNv/bi9leHBvcnQtZmls/ZS0xLnBuZw" alt="export as text" class="exportDeck">
+                                            </form>
+                                        <?php }else if(unserialize($_SESSION["user"])->getID() == $row["codUtente"]){?>
+                                            <form action="moveTo">
+                                                <input type="hidden" name="mazzo" value="<?php echo $row["mazzo"]?>">
+                                                <input type="hidden" name="espansione" value="<?php echo $row["espansione"]?>">
+                                                <input type="hidden" name="numero" value="<?php echo $row["numero"]?>">
+                                                <input type="hidden" name="foil" value="<?php echo $row["foil"]?>">
+                                                <input type="hidden" name="into" value="mancanti di <?php echo $row["mazzo"];?>">
+                                                <input type="hidden" name="public" value="1">
+                                                <input type="hidden" name="from" value="mazzi">
+                                                <input type="image" src="https://imgs.search.brave.com/tOlbrzqxPM8E8cIRWHPtdsVrBcMPZfG3NHK4TIWZJoc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZHJ5aWNvbnMu/Y29tL3VwbG9hZHMv/aWNvbi9wcmV2aWV3/Lzc0MTQvc21hbGxf/MXhfbGlzdC5wbmc" alt="mancante">
+                                            </form>
+                                        <?php }?>
                                     </td>
                                     <?php foreach($row as $key=>$cell): ?>
                                         <td>
