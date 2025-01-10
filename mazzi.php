0f0bf4644a8bab871b771eecce6fd09028e01427
RickyMandich <ricky.mandich@gmail.com>
2025-01-10 10:25:37 +0100

    aggiornamento 2025 01 10 10:25

--- mazzi.php
+++ mazzi.php
@@ -17,7 +17,7 @@
             <meta http-equiv="refresh" content="0; url=./login?from=<?php echo $file; ?>">
             <?php
         }else{
-        $resultSet = $GLOBALS["conn"]->query("select m.idMazzo, m.nome as mazzo, co.foil, m.public, ca.*, m.codUtente from mazzi m, carte ca, composizione co where co.idMazzo = m.id and ca.espansione = co.espansione and ca.numero = co.numero and ".(isset($_GET["mazzo"])?"m.id = ".$_GET["mazzo"]." and ":"")."(m.public = '1' or m.codUtente = ".unserialize($_SESSION["user"])->getID().")");
+        $resultSet = $GLOBALS["conn"]->query("select m.id, m.nome as mazzo, co.foil, m.public, ca.*, m.codUtente from mazzi m, carte ca, composizione co where co.idMazzo = m.id and ca.espansione = co.espansione and ca.numero = co.numero and ".(isset($_GET["mazzo"])?"m.id = ".$_GET["mazzo"]." and ":"")."(m.public = '1' or m.codUtente = ".unserialize($_SESSION["user"])->getID().")");
         $preDeck = [];
         while($line = $resultSet->fetch_assoc()){
             $row = [];
