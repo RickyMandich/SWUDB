cf9d8741f71f1eea55b595e4b8af882a4f8a3551
RickyMandich <ricky.mandich@gmail.com>
2025-01-11 20:27:50 +0100

    aggiornamento 2025 01 11 20:27

--- classi/Card.php
+++ classi/Card.php
@@ -65,7 +65,7 @@ class Card {
         $insert = $insert."(";
         foreach(get_object_vars($this) as $key=>$value){
             if (!$value) $value = 0;
-            if ($key === 'tratti') $value = (str_contains($value, " * ")?join(" * ", $value):$value);
+            if ($key === 'tratti') $value = (str_contains($value, "*")?join(" * ", $value):$value);
             if(gettype($value) === "string"){
                 $insert = $insert.'\'';
             }
