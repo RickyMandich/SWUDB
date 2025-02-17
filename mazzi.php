d24dd5e5f2d9d5cd12a1d8177eec8523f2617871
RickyMandich <ricky.mandich@gmail.com>
2025-02-17 16:21:42 +0100

    aggiornamento 2025 02 17 16:21

--- mazzi.php
+++ mazzi.php
@@ -72,9 +72,8 @@
             echo '<td><img src="./img/collezione.png" alt="Collezione"></td>';
             echo '<td><img src="https://imgs.search.brave.com/8lh3CqznYphqQs7SYu1sy98oK3cOR-SqnP2fN0vs8UQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pY29ucy52ZXJ5aWNvbi5jb20vcG5nLzEyOC9taXNjZWxsYW5lb3VzL2Vhc2Vtb2ItaWNvbi9leHBvcnQtZmlsZS0xLnBuZw" alt="Icon"></td>';
             foreach ($carta as $value) {
-                echo '<td>' . htmlspecialchars($value) . '</td>';
+                echo '<td>' . $value . '</td>';
             }
-            // Add other card attributes here
             echo '</tr>';
         }
     }
