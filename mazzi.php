55ba9a9289931409f740afe42156a6f66476a619
RickyMandich <ricky.mandich@gmail.com>
2025-02-17 16:10:37 +0100

    aggiornamento 2025 02 17 16:10

--- mazzi.php
+++ mazzi.php
@@ -51,8 +51,6 @@
         $firstCard = $orderCard[0];
         $expansion = $firstCard['espansione'];
         $number = str_pad($firstCard['numero'], 3, '0', STR_PAD_LEFT);
-        printlnd($orderCard, 0, 'orderCard');
-        printlnd($carte, 0, 'carte');
 
         if(count($orderCard) > 1){
             echo '<tr class="mazzo-header" data-mazzo-id="' . $mazzo['id'] . '">';
