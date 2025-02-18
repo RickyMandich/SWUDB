066a9efca2f6d2deda49f35b4e5482cf112457ed
RickyMandich <ricky.mandich@gmail.com>
2025-02-18 08:06:21 +0100

    aggiornamento 2025 02 18 08:06

--- mazzi.php
+++ mazzi.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>mazzi</title>
-        <!-- <link rel="stylesheet" href="css/mazzi.css"> -->
+        <link rel="stylesheet" href="css/profilo.css">
         <!-- <link rel="stylesheet" href="css/cartaPopUp.css"> -->
     </head>
     <?php
