d96b3f8572a1633846b3948423d5bc69a8e8b90d
Riccardo Mandich <riccardo.mandich@ptpvenezia.edu.it>
2025-02-21 12:56:37 +0100

    aggiornamento 2025 02 21 12:56

--- requestCard.php
+++ requestCard.php
@@ -3,8 +3,8 @@ require_once "header.php";
 if(isset($_GET["set"]) and isset($_GET["num"])){
     $carta = $GLOBALS["conn"]->query("select * from carte where espansione = '".$_GET["set"]."' and numero = ".$_GET["num"])->fetch_assoc();
     if($carta){
-        header('Content-Type: application/json');
-        header('Access-Control-Allow-Origin: *');
+        // header('Content-Type: application/json');
+        // header('Access-Control-Allow-Origin: *');
         echo json_encode($carta);
     }else{
         echo "carta non valida<br>";
