411f22a45b1761fd03a329129821baa8975bc527
Riccardo Mandich <riccardo.mandich@ptpvenezia.edu.it>
2025-02-21 13:22:48 +0100

    aggiornamento 2025 02 21 13:22

--- carte.php
+++ carte.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $file; ?></title>
     <link rel="stylesheet" href="./css/cartaPopUp.css">
-    <link rel="stylesheet" href="css/mazzi.css">
+    <link rel="stylesheet" href="css/profilo.css">
 </head>
 <?php
     require_once "orderCard.php";
