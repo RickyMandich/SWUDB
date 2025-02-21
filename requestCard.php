36d883b66ce8387144a79f92ac750a389858df80
Riccardo Mandich <riccardo.mandich@ptpvenezia.edu.it>
2025-02-21 12:51:48 +0100

    aggiornamento 2025 02 21 12:51

---
+++ requestCard.php
@@ -0,0 +1,11 @@
+<?php
+require_once "header.php";
+if(isset($_GET["set"]) and isset($_GET["num"])){
+    $carta = $GLOBALS["conn"]->query("select * from carte where espansione = '".$_GET["set"]."' and numero = ".$_GET["num"])->fetch_assoc();
+    if($carta){
+        echo json_encode($carta);
+    }else{
+        echo "carta non valida<br>";
+        var_dump($carta);
+    }
+}
