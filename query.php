d954640b737fb957e17a2cbac4f37110a9b63361
RickyMandich <ricky.mandich@gmail.com>
2025-02-23 17:24:28 +0100

    aggiornamento 2025 02 23 17:24

--- query.php
+++ query.php
@@ -52,7 +52,7 @@ require_once "header.php";?>
                                             <?php foreach($resultSet as $value): ?>
                                                 <td>
                                                     <?php if(isset($resultSet["nome"]) and isset($resultSet["espansione"]) and isset($resultSet["numero"])){?>
-                                                    <a href="<?php echo "https://starwarsunlimited.com/it/cards?cid=/" . $resultSet["espansione"] . "/" . sprintf("%0" . $numeri[$resultSet["espansione"]] . "d", $resultSet["numero"]);?>" target="_blank">
+                                                    <a href="<?php echo "https://starwarsunlimited.com/it/cards?cid=/" . $carta["cid"] . "?locale=it"?>" target="_blank">
                                                         <?php echo "$value";
                                                             if($value === $resultSet["nome"]){
                                                                 ?><img class="card-hover" src="https://swudb.com/images/cards/<?php echo $resultSet["espansione"] . "/" . sprintf("%0" . $numeri[$resultSet["espansione"]] . "d", $resultSet["numero"]);?>.png"><?php
