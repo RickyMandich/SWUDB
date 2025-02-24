6f61c848e00181afdc2f3cc7de8eb9b68f2b4c94
RickyMandich <ricky.mandich@gmail.com>
2025-02-24 13:08:11 +0100

    aggiornamento 2025 02 24 13:08

--- api/card.php
+++ api/card.php
@@ -1,13 +1,15 @@
 <?php
 require_once "../header.php";
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 if(isset($_GET["espansione"]) and isset($_GET["numero"])){
     $carta = $GLOBALS["conn"]->query("select * from carte where espansione = '".$_GET["espansione"]."' and numero = ".$_GET["numero"])->fetch_assoc();
     if($carta){
-        header('Content-Type: application/json');
-        header('Access-Control-Allow-Origin: *');
         echo json_encode($carta);
     }else{
         echo "carta non valida<br>";
         var_dump($carta);
     }
+}else{
+    echo json_encode(["error"=>["type"=>"wrongParams", "message"=>"parametri non validi, ho bisogno di espansione e numero"]]);
 }
