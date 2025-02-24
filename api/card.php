69447f74ca9ae7e5f9f736be8aa7bfc307c9bf26
RickyMandich <ricky.mandich@gmail.com>
2025-02-24 12:48:12 +0100

    aggiornamento 2025 02 24 12:48

--- api/card.php
+++ api/card.php
@@ -1,7 +1,7 @@
 <?php
 require_once "header.php";
-if(isset($_GET["set"]) and isset($_GET["num"])){
-    $carta = $GLOBALS["conn"]->query("select * from carte where espansione = '".$_GET["set"]."' and numero = ".$_GET["num"])->fetch_assoc();
+if(isset($_GET["espansione"]) and isset($_GET["numero"])){
+    $carta = $GLOBALS["conn"]->query("select * from carte where espansione = '".$_GET["espansione"]."' and numero = ".$_GET["numero"])->fetch_assoc();
     if($carta){
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *');
