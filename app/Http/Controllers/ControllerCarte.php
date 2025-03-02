ae915ec764b8eb4ba8e3bc6b78ad2df3d1682ca2
RickyMandich <ricky.mandich@gmail.com>
2025-03-02 13:56:06 +0100

    aggiornamento 2025 03 02 13:56

---
+++ app/Http/Controllers/ControllerCarte.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class ControllerCarte extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request){
+        $get = $request->all();
+        return view('carte');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerCarte;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/carte', [ControllerCarte::class, 'index']);
