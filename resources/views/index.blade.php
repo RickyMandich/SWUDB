86f701ba00bd92a5b4fc5ae79c1c9fafdb108ddb
RickyMandich <ricky.mandich@gmail.com>
2025-03-02 13:31:06 +0100

    aggiornamento 2025 03 02 13:31

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+    </head>
+    <body>
+
+    </body>
+</html>
+<style>
+    html{
+        background-image: url("https://imgs.search.brave.com/CNHJWzk1nYPcuPCXw6SGBNXNGrTQPDCx9_2XcXqkygI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yYXJl/LWdhbGxlcnkuY29t/L21vY2FoYmlnLzc0/ODE5LVN0YXItV2Fy/cy1BbmFraW4tU2t5/d2Fsa2VyLU9iaS1X/YW4tS2Vub2JpLUFo/c29rYS5qcGc");
+        background-size: cover;
+        background-position: center bottom;
+        height: 100%;
+        background-repeat: no-repeat;
+    }
+</style>
--- resources/views/welcome.blade.php
+++
@@ -1,4 +0,0 @@
-hello there
-<br>
-<br>
-general kenobi
