a46e888fc2e4cc36a269e3e038c43e0192dcc519
RickyMandich <ricky.mandich@gmail.com>
2025-03-03 14:50:51 +0100

    aggiornamento 2025 03 03 14:50

--- orderCard.php
+++ orderCard.php
@@ -204,7 +204,7 @@
         }
         
         // Se tipo specifico è uguale, confronto per costo (in ordine crescente)
-        if($el1["tipo"] === "Leader"){
+        if($el1["tipo"] != "Leader"){
             if ($el1["costo"] < $el2["costo"]) {
                 if($verbose){
                     echo $el1["nome"]." viene prima di ".$el2['nome']." sulla base del costo<br>";
