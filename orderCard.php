593c12107bbf384efc47d2a1b756b2da103cd600
RickyMandich <ricky.mandich@gmail.com>
2025-03-04 16:07:45 +0100

    aggiornamento 2025 03 04 16:07

--- orderCard.php
+++ orderCard.php
@@ -19,7 +19,7 @@
         $genericTipoOrder = ['Leader', 'Base'];
         
         // Definisco l'ordine degli aspetti primari
-        $primaryAspectOrder = ['Blu', 'Verde', 'Rosso', 'Giallo'];
+        $primaryAspectOrder = ['Blu', 'Verde', 'Rosso', 'Giallo', "Nero", "Bianco"];
 
         // Definisco l'ordine dei tipi specifici
         $specificTipoOrder = ['Unità', 'Miglioria', 'Evento'];
