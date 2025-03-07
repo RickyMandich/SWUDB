aceaa8e0e6d5d37756a2c25597f9a683fe13d99c
RickyMandich <ricky.mandich@gmail.com>
2025-03-07 08:36:48 +0100

    aggiornamento 2025 03 07 08:36

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Card extends Model
-{
-    //
+class Card extends Model{
+    public $incrementing = false;
+    protected $primaryKey = 'cid';
 }
