bfca970ca010fd11d87011521fc145769c71f422
Riccardo Mandich <riccardo.mandich@ptpvenezia.edu.it>
2025-04-28 12:31:06 +0200

    aggiornamento 2025 04 28 12:31

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,4 @@
 @extends('layouts.app')
-@section('content')
-    <body>
-        
-    </body>
-@endsection
 <style>
     body{
         background-image: url("https://imgs.search.brave.com/CNHJWzk1nYPcuPCXw6SGBNXNGrTQPDCx9_2XcXqkygI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yYXJl/LWdhbGxlcnkuY29t/L21vY2FoYmlnLzc0/ODE5LVN0YXItV2Fy/cy1BbmFraW4tU2t5/d2Fsa2VyLU9iaS1X/YW4tS2Vub2JpLUFo/c29rYS5qcGc");
