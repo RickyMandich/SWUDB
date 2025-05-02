3f31960635f5d93d887856f2578e02ee4df5965c
RickyMandich <ricky.mandich@gmail.com>
2025-05-02 22:20:41 +0200

    aggiornamento 2025 05 02 22:20

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SWUDB') }}</title>
+    <title>@yield('title', config('app.name', 'SWUDB'))</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -34,7 +34,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.domain', 'SWUDB.net') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
