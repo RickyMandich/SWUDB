ab07d36be537100a05c338c0843fbb6a47835d16
RickyMandich <ricky.mandich@gmail.com>
2025-05-12 18:59:39 +0200

    aggiornamento 2025 05 12 18:59

--- resources/views/carte/index.blade.php
+++ resources/views/carte/index.blade.php
@@ -8,12 +8,12 @@
                 <input type="text" placeholder="Carta" name="nome" id="nome" class="form-control" value="{{ $nome }}">
             </span>
             <input type="submit" value="Cerca" class="btn btn-primary col-1">
-            <a href="{{ route("carte") }}" class="btn btn-secondary col-1 m-2">Cancella parametri di ricerca</a>
             @foreach ($espansioni as $set)
-                <a href="{{ route("carte.set", ["espansione" => $set->espansione]) }}" class="btn btn-secondary col-1 m-2 text-center">
-                    {{ $set->espansione }}
-                </a>
+            <a href="{{ route("carte.set", ["espansione" => $set->espansione]) }}" class="btn btn-secondary col-3 col-sm-1 m-2 text-center">
+                {{ $set->espansione }}
+            </a>
             @endforeach
+            <a href="{{ route("carte") }}" class="btn btn-secondary col-9 m-4">Cancella parametri di ricerca</a>
         </form>
     </div>
     <p>Trovati {{ count($content) }} risultati</p>
@@ -26,7 +26,7 @@
                         <a href="{{ route("carta", ["espansione" => $carta->espansione, "numero" => $carta->numero]) }}">
                             <div class="row">
                                 <div class="col">
-                                    <img class="col-12" src="{{ $carta->backArt != "" ? $carta->backArt : $carta->frontArt }}" alt="immagine di {{$carta->snippet}}">
+                                    <img class="col-12" src="{{ $carta->frontArt }}" alt="immagine di {{$carta->snippet}}">
                                 </div>
                                 <div class="col">
                                     <h5>{{ $carta->snippet }}</h5>
@@ -44,7 +44,7 @@
                                         <span class="m-auto text-primary align-self-end col-3">{{ $carta->vita }}</span> <br>
                                     </div>
                                     <div class="row text-center">
-                                        <span class="col-12 {{ toClass($carta->rarita) }}">{{ $carta->rarita }}</span>
+                                        <span class="col-12 {{ toCssClass($carta->rarita) }}">{{ $carta->rarita }}</span>
                                     </div>
                                 </div>
                             </div>
@@ -57,7 +57,7 @@
 @endsection
 @section("php")
 <?php
-function toClass($class){
+function toCssClass($class){
     return str_replace(" ", "-", $class);
 }
 ?>
