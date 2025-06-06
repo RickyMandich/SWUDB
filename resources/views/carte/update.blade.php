45b1108b86d5a1aa9d33b96b4e4b743fb1f9a810
Riccardo Mandich <riccardo.mandich@ptpvenezia.edu.it>
2025-06-06 10:50:11 +0200

    aggiornamento 2025 06 06 10:50

--- resources/views/carte/update.blade.php
+++ resources/views/carte/update.blade.php
@@ -1,48 +1,41 @@
-<?php
-use App\Http\Controllers\CardsController;?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Update</title>
-    </head>
-    <body>
-        aggiornamento eseguito
-        <br>
-        {{ $count }} carte aggiunte
-        <br>
-        <?php function printlnd($line, $deep = 0, $name, $link = false){
-            if(gettype($line) == 'array' || gettype($line) == 'object'){
-                if(array_key_exists("cid", $line)){
-                    ?>
-                    <a href="{{ route('carta', ['espansione' => $line["espansione"], 'numero' => $line["numero"]]) }}">{{$name}}</a>-->{<br>
-                    <?php
-                }else{
-                    echo "$name-->{<br>";
-                }
-                foreach($line as $i => $value){
-                    unset($j);
-                    for($j = 0;$j<=$deep;$j++){
-                        echo "&nbsp;&nbsp;";
-                    }
-                    printlnd($value, $deep+1, $i);
-                }
+@extends('layouts.app')
+@section('title', 'Aggiornamento carte')
+@section('content')
+    <?php use App\Http\Controllers\CardsController;?>
+    aggiornamento eseguito
+    <br>
+    {{ $count }} carte aggiunte
+    <br>
+    <?php function printlnd($line, $deep = 0, $name, $link = false){
+        if(gettype($line) == 'array' || gettype($line) == 'object'){
+            if(array_key_exists("cid", $line)){
+                ?>
+                <a href="{{ route('carta', ['espansione' => $line["espansione"], 'numero' => $line["numero"]]) }}">{{$name}}</a>-->{<br>
+                <?php
+            }else{
+                echo "$name-->{<br>";
+            }
+            foreach($line as $i => $value){
                 unset($j);
-                for($j = 0;$j<$deep;$j++){
+                for($j = 0;$j<=$deep;$j++){
                     echo "&nbsp;&nbsp;";
                 }
-                echo "}<br>";
-            }else{
-                try{
-                    echo "$name=>$line";
-                }catch(Error $e){
-                    echo "Errore: " . $e->getMessage();
-                    echo gettype($line);
-                }
-                echo "<br>";
+                printlnd($value, $deep+1, $i);
             }
-        }?>
-        <?php printlnd($data, 0, "data", false) ?>
-    </body>
-</html>
+            unset($j);
+            for($j = 0;$j<$deep;$j++){
+                echo "&nbsp;&nbsp;";
+            }
+            echo "}<br>";
+        }else{
+            try{
+                echo "$name=>$line";
+            }catch(Error $e){
+                echo "Errore: " . $e->getMessage();
+                echo gettype($line);
+            }
+            echo "<br>";
+        }
+    }?>
+    <?php printlnd($data, 0, "data", false) ?>
+@endsection
