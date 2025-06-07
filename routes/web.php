8a01b701733afc00e7fea3f7b078867c44aef0d6
RickyMandich <ricky.mandich@gmail.com>
2025-06-07 15:16:24 +0200

    aggiornamento 2025 06 07 15:16

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,6 @@ route::get("test", function(Request $request){
     if(isset($get["espansione1"]) and isset($get["numero1"]) and isset($get["espansione2"]) and isset($get["numero2"])){
         $card1 = Card::where("espansione", $get["espansione1"])->where("numero", $get["numero1"])->first();
         $card2 = Card::where("espansione", $get["espansione2"])->where("numero", $get["numero2"])->first();
-        $cards = [$card1, $card2];
-        CardsController::mergeSort($cards, true);
+        CardsController::compareElements($card1, $card2, true);
     }
 })->name("test");
