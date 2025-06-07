6b46510243ff46cdf9d51854632870b7b821b74e
RickyMandich <ricky.mandich@gmail.com>
2025-06-07 15:28:41 +0200

    aggiornamento 2025 06 07 15:28

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -341,24 +341,22 @@ class CardsController extends Controller
         }
         
         // Se tipo specifico è uguale, confronto per costo (in ordine crescente)
-        if($el1["tipo"] != "Leader"){
-            if ($el1["costo"] < $el2["costo"]) {
-                if($verbose){
-                    echo $el1["nome"]." viene prima di ".$el2['nome']." sulla base del costo<br>";
-                }
-                return -1;
-            }
-            
-            if ($el1["costo"] > $el2["costo"]) {
-                if($verbose){
-                    echo $el2["nome"]." viene prima di ".$el1['nome']." sulla base del costo<br>";
-                }
-                return 1;
-            }
-
+        if ($el1["costo"] < $el2["costo"]) {
             if($verbose){
-                echo "le carte hanno lo stesso costo (".$el1["costo"].")<br>";
+                echo $el1["nome"]." viene prima di ".$el2['nome']." sulla base del costo<br>";
             }
+            return -1;
+        }
+        
+        if ($el1["costo"] > $el2["costo"]) {
+            if($verbose){
+                echo $el2["nome"]." viene prima di ".$el1['nome']." sulla base del costo<br>";
+            }
+            return 1;
+        }
+
+        if($verbose){
+            echo "le carte hanno lo stesso costo (".$el1["costo"].")<br>";
         }
         
         // Se costo è uguale, confronto per nome (in ordine alfabetico)
