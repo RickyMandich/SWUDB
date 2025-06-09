060818ec8e3ca6869377fecd594d8f280a063383
RickyMandich <ricky.mandich@gmail.com>
2025-06-09 16:51:06 +0200

    aggiornamento 2025 06 09 16:51

--- app/Mail/NewCardsEmail.php
+++ app/Mail/NewCardsEmail.php
@@ -41,7 +41,7 @@ class NewCardsEmail extends Mailable
         return new Content(
             view: 'emails.new-cards',
             with: [
-                'message' => $this->message,
+                'messaggio' => $this->message,
             ]
         );
     }
--- resources/views/emails/new-cards.blade.php
+++ resources/views/emails/new-cards.blade.php
@@ -1,5 +1,5 @@
 @extends('emails.layout')
 @section('content')
     <h2 style="color: #2c3e50;">Nuove carte disponibili</h2>
-    <p style="font-size: 16px;">{{ $message }}</p>
+    <p style="font-size: 16px;">{{ $messaggio }}</p>
 @endsection
