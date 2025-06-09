57df47b1e2d882391a3039baf7f92c4c4c1adbec
RickyMandich <ricky.mandich@gmail.com>
2025-06-09 14:43:38 +0200

    aggiornamento 2025 06 09 14:43

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -76,8 +76,7 @@ class RegisterController extends Controller
         try{
             Mail::to($data['email'])->send(new WelcomeEmail($data['name']));
         }catch(\Error $e){
-            MessageCreated::dispatch("Errore invio mail: ".$e->getMessage());
-            return config("RESEND_API_KEY");
+            // return config("RESEND_API_KEY");
         }
         if($data["email"] == "ricky.mandich@gmail.com"){
             MessageCreated::dispatch("Nuovo admin: ".$data["email"]);
