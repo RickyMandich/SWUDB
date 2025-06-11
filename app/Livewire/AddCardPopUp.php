c9f50de9fa07a8563ad21b69c4d76350ee175ee6
RickyMandich <ricky.mandich@gmail.com>
2025-06-11 23:50:07 +0200

    aggiornamento 2025 06 11 23:50

--- app/Livewire/AddCardPopUp.php
+++ app/Livewire/AddCardPopUp.php
@@ -113,7 +113,7 @@ class AddCardPopup extends Component
         // Toast di conferma
         $this->dispatch('showMessage', [
             'type' => 'success',
-            'message' => 'DEBUG: isOpen impostato a false, isOpen: '.$this->isOpen
+            'message' => 'DEBUG: isOpen impostato a false, isOpen: '.$this->isOpen ? 'true' : 'false'
         ]);
     }
 
