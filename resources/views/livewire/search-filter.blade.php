960fbbefaa3a692285f89bbec44fef9f80aeb80d
RickyMandich <ricky.mandich@gmail.com>
2025-06-11 17:18:04 +0200

    aggiornamento 2025 06 11 17:18

--- resources/views/livewire/search-filter.blade.php
+++ resources/views/livewire/search-filter.blade.php
@@ -4,7 +4,7 @@
             <h5 class="mb-0">
                 <i class="fas fa-filter me-2"></i>Filtri di Ricerca
             </h5>
-            <div class="d-flex gap-2 text-primary">
+            <div class="d-flex gap-2 text-primary-emphasis">
                 <button wire:click="resetAllFilters" class="btn btn-outline-secondary btn-sm">
                     <i class="fas fa-undo me-1"></i>Reset
                 </button>
