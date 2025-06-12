fb1925c0df59777f477325add01ed791a2975688
RickyMandich <ricky.mandich@gmail.com>
2025-06-12 22:26:46 +0200

    aggiornamento 2025 06 12 22:26

--- resources/views/livewire/deck-manager.blade.php
+++ resources/views/livewire/deck-manager.blade.php
@@ -140,8 +140,6 @@
                             </div>
                         </div>
 
-                    <!-- Seconda riga: Statistiche -->
-                    <div class="row">
                         <!-- Statistiche per Costo -->
                         <div class="col-md-12 mb-3">
                             <div class="card h-100">
@@ -155,10 +153,8 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <!-- Terza riga: Distribuzioni -->
-                    <div class="row mt-3">
+                        <!-- Distribuzione per Aspetto -->
                         <div class="col-md-4">
                             <div class="card">
                                 <div class="card-body">
