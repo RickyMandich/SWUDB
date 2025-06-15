9e9bf71e822adee474fa5646a1cad5cb59461a07
RickyMandich <ricky.mandich@gmail.com>
2025-06-15 15:49:25 +0200

    aggiornamento 2025 06 15 15:49

--- resources/views/carte/index.blade.php
+++ resources/views/carte/index.blade.php
@@ -11,47 +11,20 @@
         <div class="mb-3">
             <div class="alert alert-info d-flex align-items-center" id="results-counter">
                 <i class="fas fa-info-circle me-2"></i>
-                <span>Trovati <strong>{{ count($content) }}</strong> risultati</span>
+                <span>Caricamento carte...</span>
             </div>
         </div>
 
         <!-- Contenitore per i risultati -->
         <div id="cards-container">
             <div class="row" id="cards-grid">
-                @foreach ($content as $carta)
-                    <div class="col-12 col-sm-4 ps-4 pe-4 pt-4 pb-4 card-item">
-                        <div class="innerCarta row pr-10">
-                            <div class="col-12 col-sm-12 rounded-4 border-primary-subtle bg-secondary-subtle p-3">
-                                <a href="{{ route("carta", ["espansione" => $carta->espansione, "numero" => $carta->numero]) }}">
-                                    <div class="row">
-                                        <div class="col">
-                                            <img class="col-12" src="{{ $carta->frontArt }}" alt="immagine di {{$carta->snippet}}">
-                                        </div>
-                                        <div class="col">
-                                            <h5>{{ $carta->snippet }}</h5>
-                                            {{ $carta->tratti }} <br>
-                                            <div class="row">
-                                                <span class="col-9">costo:</span>
-                                                <span class="m-auto text-warning align-self-end col-3">{{ $carta->costo }}</span> <br>
-                                            </div>
-                                            <div class="row">
-                                                <span class="col-9">potenza:</span>
-                                                <span class="m-auto text-danger align-self-end col-3">{{ $carta->potenza }}</span> <br>
-                                            </div>
-                                            <div class="row">
-                                                <span class="col-9">vita:</span>
-                                                <span class="m-auto text-primary align-self-end col-3">{{ $carta->vita }}</span> <br>
-                                            </div>
-                                            <div class="row text-center">
-                                                <span class="col-12 {{ toCssClass($carta->rarita) }}">{{ $carta->rarita }}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
+                <!-- Le carte verranno caricate dinamicamente da Livewire -->
+                <div class="col-12 text-center py-5">
+                    <div class="alert alert-info">
+                        <i class="fas fa-search me-2"></i>
+                        Utilizza i filtri sopra per cercare le carte.
                     </div>
-                @endforeach
+                </div>
             </div>
         </div>
     </div>
