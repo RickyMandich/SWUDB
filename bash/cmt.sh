fca85f61bfbd50be668cbc5425e5d300d2ec4408
RickyMandich <ricky.mandich@gmail.com>
2025-06-16 17:53:18 +0200

    aggiornamento 2025 06 16 17:53 [v${APP_VERSION_TYPE} ${APP_VERSION_PRIMARY}.${APP_VERSION_SECONDARY}.${APP_VERSION_TERTIARY}]

--- bash/cmt.sh
+++ bash/cmt.sh
@@ -2,9 +2,16 @@
 git add .
 git status
 
-# Crea il nome del commit con data e ora
+# Leggi la versione dell'app dal file .env
+if [ -f .env ]; then
+    APP_VERSION=$(grep "^APP_VERSION=" .env | cut -d '=' -f2 | tr -d '"')
+else
+    APP_VERSION="unknown"
+fi
+
+# Crea il nome del commit con data, ora e versione
 nomeCommit=$(date "+%Y %m %d %H:%M")
-nomeCommit="aggiornamento $nomeCommit"
+nomeCommit="aggiornamento $nomeCommit [v$APP_VERSION]"
 git commit -m "$nomeCommit"
 
 # Esegui il push sul repository remoto
