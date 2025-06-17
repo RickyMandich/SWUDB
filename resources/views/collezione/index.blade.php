10c0bf0e7ed9443d1fe41d9150716295f0f56120
RickyMandich <ricky.mandich@gmail.com>
2025-06-17 12:32:31 +0200

    aggiornamento 2025 06 17 12:32 [vBETA 0.5.98]

--- resources/views/collezione/index.blade.php
+++ resources/views/collezione/index.blade.php
@@ -285,7 +285,7 @@
                                             <span class="m-auto text-primary align-self-end col-3">${carta.vita || '-'}</span> <br>
                                         </div>
                                         <div class="row text-center">
-                                            <span class="col-12 ${carta.rarita.toLowerCase().replaceAll(' ', '')}">${carta.rarita}</span>
+                                            <span class="col-12 ${carta.rarita.toLowerCase().split(' ').join('')}">${carta.rarita}</span>
                                         </div>
 
                                         <!-- Controlli per la collezione -->
