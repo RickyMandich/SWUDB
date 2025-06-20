aaf2bda7542ad864041a28ce56818b084fa76ccc
RickyMandich <ricky.mandich@gmail.com>
2025-06-20 19:46:32 +0200

    aggiornamento 2025 06 20 19:46 [vBETA 0.5.113]

--- resources/views/carte/update.blade.php
+++ resources/views/carte/update.blade.php
@@ -53,12 +53,14 @@
                                     alert.classList.remove('alert-info');
                                     alert.classList.add('alert-success');
 
-                                    // Add refresh button
-                                    const refreshBtn = document.createElement('button');
-                                    refreshBtn.className = 'btn btn-primary mt-2';
-                                    refreshBtn.textContent = 'Aggiorna pagina per vedere i risultati';
-                                    refreshBtn.onclick = () => window.location.reload();
-                                    alert.appendChild(refreshBtn);
+                                    // Add refresh button only if it doesn't exist
+                                    if (!document.querySelector('.refresh-btn')) {
+                                        const refreshBtn = document.createElement('button');
+                                        refreshBtn.className = 'btn btn-primary mt-2 refresh-btn';
+                                        refreshBtn.textContent = 'Aggiorna pagina per vedere i risultati';
+                                        refreshBtn.onclick = () => window.location.reload();
+                                        alert.appendChild(refreshBtn);
+                                    }
                                 }
                             })
                             .catch(error => {
@@ -90,40 +92,54 @@
             </div>
         @endif
     @endif
-    <?php function printlnd($line, $deep = 0, $name, $link = false){
-        if(gettype($line) == 'array' || gettype($line) == 'object'){
-            if(array_key_exists("cid", $line)){
-                ?>
-                <a href="{{ route('carta', ['espansione' => $line["espansione"], 'numero' => $line["numero"]]) }}">{{$name}}</a>-->{<br>
-                <?php
-            }else{
-                echo "$name-->{<br>";
-            }
-            foreach($line as $i => $value){
+    @if(env('APP_DEBUG') && Auth::check() && Auth::user()->admin)
+        <?php function printlnd($line, $deep = 0, $name, $link = false){
+            if(gettype($line) == 'array' || gettype($line) == 'object'){
+                if(array_key_exists("cid", $line)){
+                    ?>
+                    <a href="{{ route('carta', ['espansione' => $line["espansione"], 'numero' => $line["numero"]]) }}">{{$name}}</a>-->{<br>
+                    <?php
+                }else{
+                    echo "$name-->{<br>";
+                }
+                foreach($line as $i => $value){
+                    unset($j);
+                    for($j = 0;$j<=$deep;$j++){
+                        echo "&nbsp;&nbsp;";
+                    }
+                    printlnd($value, $deep+1, $i);
+                }
                 unset($j);
-                for($j = 0;$j<=$deep;$j++){
+                for($j = 0;$j<$deep;$j++){
                     echo "&nbsp;&nbsp;";
                 }
-                printlnd($value, $deep+1, $i);
+                echo "}<br>";
+            }else{
+                try{
+                    echo "$name=>$line";
+                }catch(Error $e){
+                    echo "Errore: " . $e->getMessage();
+                    echo gettype($line);
+                }
+                echo "<br>";
             }
-            unset($j);
-            for($j = 0;$j<$deep;$j++){
-                echo "&nbsp;&nbsp;";
-            }
-            echo "}<br>";
-        }else{
-            try{
-                echo "$name=>$line";
-            }catch(Error $e){
-                echo "Errore: " . $e->getMessage();
-                echo gettype($line);
-            }
-            echo "<br>";
-        }
-    }?>
-    @if(isset($data))
-        <?php printlnd($data, 0, "data", false) ?>
-    @else
-        <?php printlnd($output, 0, "output", false) ?>
+        }?>
+
+        <div class="mt-4">
+            <button class="btn btn-sm btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#debugInfo" aria-expanded="false">
+                <i class="fas fa-bug"></i> Debug Info (Admin)
+            </button>
+            <div class="collapse mt-2" id="debugInfo">
+                <div class="card">
+                    <div class="card-body">
+                        @if(isset($data))
+                            <?php printlnd($data, 0, "data", false) ?>
+                        @else
+                            <?php printlnd($output, 0, "output", false) ?>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
     @endif
 @endsection
