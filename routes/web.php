e172826fb603b8e82b5ead463c5cfed82fa5dd9a
RickyMandich <ricky.mandich@gmail.com>
2025-06-25 15:17:30 +0200

    aggiornamento 2025 06 25 15:17 [vBETA 0.6.5]

--- routes/web.php
+++ routes/web.php
@@ -109,5 +109,5 @@ route::get('migrate', function () {
 })->name("migrate");
 
 route::get('test', function () {
-    return file_get_contents(base_path('todo list.md'));
+    return file_get_contents('todo list.md');
 })->name("test");
