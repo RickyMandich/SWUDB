2a9a0f94ba73850825dbb56d4a56cd4447d0cd02
RickyMandich <ricky.mandich@gmail.com>
2025-06-28 09:05:14 +0200

    aggiornamento 2025 06 28 09:05 [vBETA 0.6.56]

--- resources/views/livewire/deck-manager.blade.php
+++ resources/views/livewire/deck-manager.blade.php
@@ -83,13 +83,13 @@
                     <div class="contenuto">
                         @foreach($mazzo as $id => $carta)
                             <span class="d-flex mt-4">
-                                {{ $carta['copie'] }}x
+                                {{ $carta['copie'] ?? 1 }}x
                                 @if ($proprietario)
                                     <button type="button" wire:click="aumentaCopia('{{ $id }}')" class="btn btn-success rounded-0 rounded-start-1 border-end-0 py-1 px-2 lh-1">+</button>
                                     <button type="button" wire:click="diminuisciCopia('{{ $id }}')" class="btn btn-danger rounded-0 rounded-end-1 border-start-0 py-1 px-2 lh-1">-</button>
                                 @endif
-                                <a href="{{ route('carta', ["espansione" => $carta["espansione"], "numero" => $carta["numero"]]) }}" target="_blank">
-                                    {{ $carta['snippet'] }}
+                                <a href="{{ route('carta', ["espansione" => $carta["espansione"] ?? '', "numero" => $carta["numero"] ?? 0]) }}" target="_blank">
+                                    {{ $carta['snippet'] ?? '' }}
                                 </a>
                             </span>
                         @endforeach
@@ -114,12 +114,12 @@
                         <div class="mb-4 contenuto">
                             @foreach($aggiunte as $id => $carta)
                                 <span class="d-flex mt-4">
-                                    {{ $carta['copie'] }}x
+                                    {{ $carta['copie'] ?? 1 }}x
                                     @if ($proprietario)
                                         <button type="button" wire:click="diminuisciCopia('{{ $id }}')" class="btn btn-danger rounded-1 border-0 py-1 px-2 lh-1">-</button>
                                     @endif
-                                    <a href="{{ route('carta', ["espansione" => $carta["espansione"], "numero" => $carta["numero"]]) }}" target="_blank">
-                                        {{ $carta['snippet'] }}
+                                    <a href="{{ route('carta', ["espansione" => $carta["espansione"] ?? '', "numero" => $carta["numero"] ?? 0]) }}" target="_blank">
+                                        {{ $carta['snippet'] ?? '' }}
                                     </a>
                                 </span>
                             @endforeach
@@ -130,10 +130,10 @@
                         <div class="mb-4 contenuto">
                             @foreach($rimosse as $id => $carta)
                                 <span class="d-flex mt-4">
-                                    {{ $carta['copie'] }}x
+                                    {{ $carta['copie'] ?? 1 }}x
                                     <button type="button" wire:click="aumentaCopia('{{ $id }}')" class="btn btn-success rounded-1 border-0 py-1 px-2 lh-1">+</button>
-                                    <a href="{{ route('carta', ["espansione" => $carta["espansione"], "numero" => $carta["numero"]]) }}" target="_blank">
-                                        {{ $carta['snippet'] }}
+                                    <a href="{{ route('carta', ["espansione" => $carta["espansione"] ?? '', "numero" => $carta["numero"] ?? 0]) }}" target="_blank">
+                                        {{ $carta['snippet'] ?? '' }}
                                     </a>
                                 </span>
                             @endforeach
