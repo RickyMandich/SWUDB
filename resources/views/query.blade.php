092698076d6967b8bc82277033cca94561c998d5
RickyMandich <ricky.mandich@gmail.com>
2025-07-03 15:22:03 +0200

    aggiornamento 2025 07 03 15:22 [vBETA 0.7.11]

--- resources/views/query.blade.php
+++ resources/views/query.blade.php
@@ -65,8 +65,13 @@
                                     @foreach($result as $k=>$row)
                                         <tr>
                                             @foreach((array)$row as $value)
-                                            @if(isset($sorted) and $sorted)
+                                                @if(isset($sorted) and $sorted)
+                                                    <a href="{{ route('carta', ['espansione' => $row['espansione'], 'numero' => $row['numero']]) }}" target="_blank">
+                                                @endif
                                                 <td>{!! $value !!}</td>
+                                                @if(isset($sorted) and $sorted)
+                                                    </a>
+                                                @endif
                                             @endforeach
                                         </tr>
                                     @endforeach
