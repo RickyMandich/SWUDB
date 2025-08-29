af7f6972f1b57e8b9ea6cfe62e038d29ceb10826
RickyMandich <ricky.mandich@gmail.com>
2025-08-29 17:33:38 +0200

    aggiornamento 2025 08 29 17:33 [vBETA 0.7.35]

--- resources/views/query.blade.php
+++ resources/views/query.blade.php
@@ -70,7 +70,7 @@
                                                         @if(isset($sorted) and $sorted)
                                                             <a href="{{ route('carta', ['espansione' => $row->espansione, 'numero' => $row->numero]) }}" target="_blank">
                                                         @endif
-                                                        {!! $key !!} => {!! $value !!}
+                                                        <!--{!! $key !!} => -->{!! $value !!}
                                                         @if(isset($sorted) and $sorted)
                                                             </a>
                                                         @endif
