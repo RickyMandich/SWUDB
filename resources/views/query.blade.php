4528757c1a01b22d10534c53c9d1ce143f02d0fa
RickyMandich <ricky.mandich@gmail.com>
2025-08-29 17:36:27 +0200

    aggiornamento 2025 08 29 17:36 [vBETA 0.7.36]

--- resources/views/query.blade.php
+++ resources/views/query.blade.php
@@ -53,7 +53,7 @@
                             @endif
                         </div>
                         <div class="table-responsive">
-                            <table class="table table-striped table-sm">
+                            <table class="table table-striped table-hover table-sm">
                                 <thead class="table-dark">
                                     <tr>
                                         @foreach(array_keys((array)$result[0]) as $column)
