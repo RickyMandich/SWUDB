6c588d8c983aeef33182c842052c89ad1bd7daf3
RickyMandich <ricky.mandich@gmail.com>
2025-09-22 16:50:53 +0200

    aggiornamento 2025 09 22 16:50 [vBETA 0.7.6]

--- config/hashing.php
+++ config/hashing.php
@@ -29,7 +29,7 @@ return [
     */
 
     'bcrypt' => [
-        'rounds' => env('BCRYPT_ROUNDS', 20),
+        'rounds' => env('BCRYPT_ROUNDS', 15),
         'verify' => env('HASH_VERIFY', true),
         'limit' => env('BCRYPT_LIMIT', null),
     ],
