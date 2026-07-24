8459d7c89e288e0da5c6e97646ff4a78ac494a02
RickyMandich <ricky.mandich@gmail.com>
2026-07-24 13:19:13 +0200

    aggiornamento 2026 07 24 13:19 [0.9.50]

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -1425,6 +1425,9 @@ class CardsController extends Controller
         // 0. Rotazione
 
         $set1 = Expansion::where("espansione", "=", $el1->id);
+        if ($verbose) {
+            var_dump($set1);
+        }
         $rot1 = $set1->first()->rotazione;
         $set2 = Expansion::where("espansione", "=", $el2->id);
         $rot2 = $set2->first()->rotazione;
