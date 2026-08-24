27596c809a83317fab3bd75d544e934b1907ef91
RickyMandich <ricky.mandich@gmail.com>
2026-08-24 23:09:45 +0200

    aggiornamento 2026 08 24 23:09 [0.9.75] - changed scan day to thursday

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('scan:execute')->weeklyOn(1, '00:00');
+Schedule::command('scan:execute')->weeklyOn(2, '00:00');
