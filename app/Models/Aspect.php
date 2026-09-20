339f5cb5efda1e19dd1daaea3d807df09c3c9a63
RickyMandich <ricky.mandich@gmail.com>
2026-09-20 13:49:42 +0200

    aggiornamento 2026 09 20 13:49 [1.0.0] - added aspect migration and model

--- app/Models/Aspect.php
+++ app/Models/Aspect.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aspect extends Model
 {
-    //
+    protected $table = 'aspects';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $incrementing = false;
+    protected $fillable = [
+        'id',
+        'name',
+        'slug',
+        'color',
+        'order',
+    ];
+
+    public function cards()
+    {
+        return $this->belongsToMany(Card::class, 'card_aspect', 'aspect_id', 'cid');
+    }
 }
--- database/migrations/2026_09_13_202808_create_aspects_table.php
+++ database/migrations/2026_09_13_202808_create_aspects_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('aspects', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
+            $table->string('name');
+            $table->string('color');
+            $table->string('slug')->unique();
+            $table->integer('order')->unique();
             $table->timestamps();
         });
     }
